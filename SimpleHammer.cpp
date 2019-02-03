796fdbc3878c6d407f4f138aebb287ef12b050b6
Braden Mayew <bradenmayew@yahoo.com>
2019-02-03 15:31:15 -0500

    Updated SimpleHammer.cpp
    SimpleHammer now specifies that if the armor is below 30, then the SimpleHammer
    will ignore the armor and inflict damage

--- SimpleHammer.cpp
+++ SimpleHammer.cpp
@@ -5,3 +5,23 @@
 // Assignment 1                                                   //
 // --------------------- SimpleHammer.cpp ----------------------- //
 //****************************************************************//
+
+/* 
+ * File:   CommonSpear.cpp
+ * Author: Javier <jrescobara@gmail.com>
+ * 
+ * Created on September 27, 2017, 4:15 PM
+ */
+
+#include "SimpleHammer.h"
+
+double SimpleHammer::hit(double armor) {
+	double damage = 0.0;
+    if (armor < 30) {
+    	damage = hitPoints;
+    	if (damage < 0) {
+    		return 0;
+    	}
+    	return damage;
+    }
+}
