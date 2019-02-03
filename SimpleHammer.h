7a2ff764bbb60ed54b191c5fb50d83790db91da4
Braden Mayew <bradenmayew@yahoo.com>
2019-02-03 15:29:55 -0500

    Updated SimpleHammer.h
    Added constructor SimpleHammer, implemented with Weapon class. Constitutes 25hp. Added a destructor
    and added hit function that takes a double parameter armor

--- SimpleHammer.h
+++ SimpleHammer.h
@@ -5,3 +5,27 @@
 // Assignment 1                                                   //
 // ---------------------- SimpleHammer.h ------------------------ //
 //****************************************************************//
+
+#include <string>
+#include "Weapon.h"
+
+#ifndef SIMPLEHAMMER_H
+#define SIMPLEHAMMER_H
+
+/**
+ * Defines the behavior of a Simple Hammer (hitpoints = 25. If the 
+ * armor is < 30, the hammer will ignore all the armor points)
+ */
+class SimpleHammer : public Weapon {
+public:
+
+    SimpleHammer() : Weapon("Simple Hammer", 25.0) { //Calls Weapon(name, hitpoints) constructor with values Simple Hammer 25hp
+    }
+
+    virtual ~SimpleHammer() {};
+
+    virtual double hit(double armor);
+
+};
+
+#endif /* SIMPLEHAMMER_H */
