7d13e55dd6c1305e2a161dfc9b99cbe3683da067
Braden Mayew <bradenmayew@yahoo.com>
2019-02-03 16:58:32 -0500

    Updated CrazyRandomSword
    Added cstdlib for rand function, math.h for floor function in order to utilize rand
    which requires an integer

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -6,18 +6,15 @@
 // ------------------- CrazyRandomSword.cpp --------------------- //
 //****************************************************************//
 
-/* 
- * File:   CommonSpear.cpp
- * Author: Javier <jrescobara@gmail.com>
- * 
- * Created on September 27, 2017, 4:15 PM
- */
 
+#include <cstdlib>
+#include <math.h>
 #include "CrazyRandomSword.h"
 
 double CrazyRandomSword::hit(double armor) {
-    hitpoints = rand() % 100 + 7; // random number between 7 and 100
-    armor = rand() % (armor / 3) + 2; // random number between 2 and 1/3 of the armor
+    int number = floor (armor/3.0);
+    hitPoints = rand() % 100 + 7; // random number between 7 and 100
+    armor = rand() % number + 2; // random number between 2 and 1/3 of the armor
 	double damage = hitPoints - armor;
     if(damage < 0){
         return 0;
