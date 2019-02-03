a381c056f0dc3eb42559cb97e7ac90538b2ba611
Braden Mayew <bradenmayew@yahoo.com>
2019-02-03 17:00:27 -0500

    Updated CrazyRandomSword
    Moved random armor hp generation to the top of the function

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -12,9 +12,9 @@
 #include "CrazyRandomSword.h"
 
 double CrazyRandomSword::hit(double armor) {
+    armor = rand() % number + 2; // random number between 2 and 1/3 of the armor
     int number = floor (armor/3.0);
     hitPoints = rand() % 100 + 7; // random number between 7 and 100
-    armor = rand() % number + 2; // random number between 2 and 1/3 of the armor
 	double damage = hitPoints - armor;
     if(damage < 0){
         return 0;
