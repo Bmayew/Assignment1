639957652a7a1c30a13ee5e1df46364d827eae13
Braden Mayew <bradenmayew@yahoo.com>
2019-02-03 19:26:40 -0500

    Updated SimpleHammer.cpp

--- SimpleHammer.cpp
+++ SimpleHammer.cpp
@@ -6,22 +6,12 @@
 // --------------------- SimpleHammer.cpp ----------------------- //
 //****************************************************************//
 
-/* 
- * File:   CommonSpear.cpp
- * Author: Javier <jrescobara@gmail.com>
- * 
- * Created on September 27, 2017, 4:15 PM
- */
-
 #include "SimpleHammer.h"
 
 double SimpleHammer::hit(double armor) {
 	double damage = 0.0;
     if (armor < 30) {
     	damage = hitPoints;
-    	if (damage < 0) {
-    		return 0;
-    	}
     	return damage;
     } else {
     	damage = hitPoints - armor;
